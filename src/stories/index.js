0b67a7193380bd12f423e7b53051a793d46cfaa4
Aborysa <aborysa@gmail.com>
2019-07-07 18:10:15 +0200

    Chore - Move ExampleBox and colors
    Move ExampleBox from './src' to './src/components'
    Move colors from './src' to './src/common'

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import ExampleBox from '../ExampleBox';
+import ExampleBox from 'components/ExampleBox';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
