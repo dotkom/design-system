0bd7d190860ca1c633a0029d162aabf031efdea9
Aslak Hollund <aslak.hollund@iterate.no>
2019-07-07 21:48:49 +0200

    create fonts

--- src/ExampleBox.tsx
+++ src/ExampleBox.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from './colors';
+import { HeadingLarge, Paragrah, HeadingSmall } from './global';
 
 const ExampleBox = () => {
   const exampleProp = true;
 
-  return <StyledBox someProp={exampleProp}>Heisann</StyledBox>;
+  return (
+    <>
+      <HeadingLarge>HeadingLarge</HeadingLarge>
+      <Paragrah>Paragrah</Paragrah>
+      <HeadingSmall>HeadingSmall</HeadingSmall>
+      <StyledBox someProp={exampleProp}>Heisann</StyledBox>
+    </>
+  );
 };
 
 export default ExampleBox;
@@ -13,4 +21,4 @@ export default ExampleBox;
 const StyledBox = styled.div<{ someProp: boolean }>`
   ${({ someProp }) => (someProp ? 'color: gray' : null)}
   background-color: ${colors.systemWarning50};
-`;
+  `;
--- src/global.tsx
+++ src/global.tsx
@@ -2,7 +2,37 @@ import styled, { createGlobalStyle } from 'styled-components';
 import React from 'react';
 
 const GlobalStyles = createGlobalStyle`
+`;
 
+export const HeadingLarge = styled.h1`
+  font-family: 'Source Serif Pro', serif;
+  font-size: 3rem;
+`;
+
+export const HeadingMedium = styled.h2`
+  font-family: 'Source Serif Pro', serif;
+  font-size: 2.5rem;
+`;
+
+export const HeadingSmall = styled.h3`
+  font-family: 'Source Serif Pro', serif;
+  font-size: 2rem;
+`;
+
+export const Paragrah = styled.p`
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: 16px;
+`;
+
+export const Link = styled.p`
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Description = styled.p`
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: 11px;
 `;
 
 export const globalStylesDecorator = (storyFn: any) => {
