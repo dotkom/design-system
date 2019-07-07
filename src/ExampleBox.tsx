ec7ad4d77275e66dfb8d7406b6453ce5b932d4ce
Aslak Hollund <aslak.hollund@iterate.no>
2019-07-07 16:54:00 +0200

    example styledcomponent

---
+++ src/ExampleBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from './colors';
+
+const ExampleBox = () => {
+  const exampleProp = true;
+
+  return <StyledBox someProp={exampleProp}>Heisann</StyledBox>;
+};
+
+export default ExampleBox;
+
+const StyledBox = styled.div<{ someProp: boolean }>`
+  ${({ someProp }) => (someProp ? 'color: gray' : null)}
+  background-color: ${colors.hotpink};
+`;
