222bd9e5e2044f3a6652b3998c3db45e2858a3b2
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-09-05 14:37:05 +0200

    Create story book samples of the input field

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,6 +6,7 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import ExampleBox from 'components/ExampleBox';
+import Input from 'components/input/Input';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -19,4 +20,31 @@ storiesOf('Button', module)
     </Button>
   ));
 
-storiesOf('TestBox', module).add('red', () => <ExampleBox>hi</ExampleBox>);
+storiesOf('Input', module).add('input field', () => (
+  <>
+    <h3>Input</h3>
+    <Input placeholder={'Search...'} />
+    <h3>Input with label</h3>
+    <Input placeholder={'Search...'} label="Label" />
+    <h3>Disabled</h3>
+    <Input defaultValue={'disabled'} disabled />
+    <h3>Input with error</h3>
+    <Input defaultValue={'Not an email'} type="email" pattern=".+@.+" required errorMessage="Not an email" />
+    <h3>Input with error (colored)</h3>
+    <Input
+      defaultValue={'Green error message'}
+      type="email"
+      pattern=".+@.+"
+      required
+      invalidColor={'green'}
+      errorMessage="Not a valid email"
+    />
+    <h3>Input with dynamic error message</h3>
+    <Input
+      defaultValue={'Text'}
+      required
+      pattern=".{10,}"
+      errorMessage={(input: string) => `You need ${10 - input.length} more letter${input.length !== 9 ? 's' : ''}.`}
+    />
+  </>
+));
