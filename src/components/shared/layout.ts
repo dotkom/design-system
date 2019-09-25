19783288047d3ef2fc6ef36c72d0c50cb7b47c3c
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-09-26 01:46:20 +0200

    Add break points and media queries

---
+++ src/components/shared/layout.ts
@@ -0,0 +1,29 @@
+export const breakPoints = {
+  minMobileWidth: '0px',
+  maxMobileWidth: '755px',
+  minTabletWidth: '756px',
+  maxTabletWidth: '1199px',
+  minDesktopWidth: '1200px',
+  maxDesktopWidth: '1704px',
+};
+
+export const media = {
+  mobile: `screen and (min-width: ${breakPoints.minMobileWidth}) and (max-width: ${breakPoints.maxMobileWidth})`,
+  tablet: `screen and (min-width: ${breakPoints.minTabletWidth}) and (max-width: ${breakPoints.maxTabletWidth})`,
+  desktop: `screen and (min-width: ${breakPoints.minDesktopWidth}) and (max-width: ${breakPoints.maxDesktopWidth})`,
+  not: {
+    mobile: `screen and (min-width: ${breakPoints.minTabletWidth})`,
+    tablet: `screen and (max-width: ${breakPoints.maxMobileWidth}), screen and (min-width: ${breakPoints.minDesktopWidth})`,
+    desktop: `screen and (max-width: ${breakPoints.maxDesktopWidth})`,
+  },
+  min: {
+    mobile: `screen and (min-width: ${breakPoints.minMobileWidth})`,
+    tablet: `screen and (min-width: ${breakPoints.minTabletWidth})`,
+    desktop: `screen and (min-width: ${breakPoints.minDesktopWidth})`,
+  },
+  max: {
+    mobile: `screen and (max-width: ${breakPoints.maxMobileWidth})`,
+    tablet: `screen and (max-width: ${breakPoints.maxTabletWidth})`,
+    desktop: `screen and (max-width: ${breakPoints.maxDesktopWidth})`,
+  },
+};
