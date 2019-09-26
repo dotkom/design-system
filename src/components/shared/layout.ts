ec6c8891ec5dab79a92f524f897bac1ca1d2785d
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-09-26 03:34:40 +0200

    Add small and large desktop as it is a logical split

--- src/components/shared/layout.ts
+++ src/components/shared/layout.ts
@@ -11,15 +11,21 @@ export const breakPoints = {
 export const media = {
   mobile: `screen and (min-width: ${breakPoints.minMobileWidth}) and (max-width: ${breakPoints.maxMobileWidth})`,
   tablet: `screen and (min-width: ${breakPoints.minTabletWidth}) and (max-width: ${breakPoints.maxTabletWidth})`,
-  desktop: `screen and (min-width: ${breakPoints.minDesktopWidth}) and (max-width: ${breakPoints.maxDesktopWidth})`,
+  desktop: `screen and (min-width: ${breakPoints.minDesktopWidth})`,
+  smallDesktop: `screen and (min-width: ${breakPoints.minDesktopWidth}) and (max-width: ${breakPoints.maxDesktopWidth})`,
+  largeDesktop: `screen and (min-width: ${breakPoints.minLargeDesktopWidth})`,
   min: {
     mobile: `screen and (min-width: ${breakPoints.minMobileWidth})`,
     tablet: `screen and (min-width: ${breakPoints.minTabletWidth})`,
     desktop: `screen and (min-width: ${breakPoints.minDesktopWidth})`,
+    smallDesktop: `screen and (min-width: ${breakPoints.minDesktopWidth})`,
+    largeDesktop: `screen and (min-width: ${breakPoints.minLargeDesktopWidth})`,
   },
   max: {
     mobile: `screen and (max-width: ${breakPoints.maxMobileWidth})`,
     tablet: `screen and (max-width: ${breakPoints.maxTabletWidth})`,
-    desktop: `screen and (max-width: ${breakPoints.maxDesktopWidth})`,
+    desktop: `screen`,
+    smallDesktop: `screen and (max-width: ${breakPoints.maxDesktopWidth})`,
+    largeDesktop: `screen`,
   },
 };
