5d756e674d133f0d3b822fb99add124cbc3753af
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-09-26 02:49:16 +0200

    Add more docs to media break points

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
  * importing them as `import { Component } from "package-name";`
  */
 
+import { colors } from './common/colors';
 import * as global from './components/shared/global';
-export { global };
+import { media, breakPoints } from './components/shared/layout';
+export { colors, global, media, breakPoints };
 
 export { default as Alert } from './components/alerts/Alert';
 export { default as TextField } from './components/forms/TextField';
