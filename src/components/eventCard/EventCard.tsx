929b1891d25456c5d6684b2a2ee339599448618e
anneork <Inepus10>
2019-10-02 22:09:13 +0200

    Event card for the frontpage

--- src/components/eventCard/EventCard.tsx
+++ src/components/eventCard/EventCard.tsx
@@ -9,15 +9,26 @@ interface ICard {
   eventColor: string;
   date: string;
   attending: string;
+  imgSrc: string;
+  isLargeEvent: boolean;
+  category: string;
 }
 
 const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Open Sans Pro', sans-serif;
+`;
+
+const EventInfo = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
   min-height: 4rem;
   width: 100%;
+  border-radius: 3px;
+  background: #fff;
 `;
 
 const ColorCodeIndicator = styled.span<{ color?: string }>`
@@ -44,20 +55,50 @@ const Box = styled.span`
   }
 `;
 
-const EventCard = ({ eventName, eventColor, date, attending }: ICard) => (
+const Picture = styled.img<{ hidden: boolean }>`
+  width: 100%;
+  border-bottom: 0.5px solid #b7b7b7;
+  &:hidden {
+    display: none;
+  }
+`;
+
+const CategoryInfo = styled.h2<{ color?: string; hidden: boolean }>`
+  background: ${({ color }) => color || colors.primay};
+  color: #fff;
+  margin: 0;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  padding: 7px 0;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  &:hidden {
+    display: none;
+  }
+`;
+
+const EventCard = ({ eventName, eventColor, date, attending, imgSrc = '', isLargeEvent, category }: ICard) => (
   <CardContainer>
-    <Box>
-      <ColorCodeIndicator color={eventColor}></ColorCodeIndicator>
-      <Text>{eventName}</Text>
-    </Box>
-    <Box>
-      <DateRangeIcon />
-      <Text>{date}</Text>
-    </Box>
-    <Box>
-      <PermIdentityIcon />
-      <Text>{attending}</Text>
-    </Box>
+    <CategoryInfo color={eventColor} hidden={!isLargeEvent}>
+      {category}
+    </CategoryInfo>
+    <Picture src={imgSrc} hidden={!isLargeEvent}></Picture>
+    <EventInfo>
+      <Box>
+        <ColorCodeIndicator color={eventColor}></ColorCodeIndicator>
+        <Text>{eventName}</Text>
+      </Box>
+      <Box>
+        <DateRangeIcon />
+        <Text>{date}</Text>
+      </Box>
+      <Box>
+        <PermIdentityIcon />
+        <Text>{attending}</Text>
+      </Box>
+    </EventInfo>
   </CardContainer>
 );
 
