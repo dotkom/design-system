9dc526ef1de1b561ba0358aa86a848f6a0567a7f
annemoss <anneork@stud.ntnu.no>
2019-10-09 20:34:27 +0200

    added hover and disabled effect

--- src/components/radio/Radio.tsx
+++ src/components/radio/Radio.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { colors } from 'common/colors';
 
 interface RadioProps {
   labels: string[];
   groupName: string;
+  disabled?: boolean;
 }
 
-const Radio = ({ labels, groupName }: RadioProps) => {
+const Radio = ({ labels, groupName, disabled }: RadioProps) => {
   const radios = labels.map((label) => (
-    <RadioLabel>
+    <RadioLabel disabled={disabled}>
       <RadioButton name={groupName} />
       {label}
     </RadioLabel>
@@ -20,8 +22,20 @@ const RadioButton = styled.input.attrs({ type: 'radio' })`
   margin-right: 0.5rem;
 `;
 
-const RadioLabel = styled.label`
+const RadioLabelDisabled = css`
+  color: ${colors.graysGray};
+  cursor: not-allowed;
+  &:hover {
+    color: ${colors.graysGray};
+  }
+`;
+const RadioLabel = styled.label<{ disabled?: boolean }>`
   display: block;
+  cursor: pointer;
+  &:hover {
+    color: ${colors.primary};
+  }
+  ${({ disabled }) => disabled && RadioLabelDisabled};
 `;
 
 const RadioGroup = styled.div`
