c69902551c017528aa9dabd458b1a9eafc62947b
MartinLunde <martinlunde96@gmail.com>
2019-10-09 22:05:54 +0200

    Fix linting

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -3,21 +3,21 @@ import styled from 'styled-components';
 import { colors } from 'common/colors';
 
 interface CardProps {
-    children: React.ReactNode;
-    [name: string]: any;
+  children: React.ReactNode;
+  [name: string]: any;
 }
 
 const StyledCard = styled.div`
   display: block;
   background-color: ${colors.graysWhite};
   width: fit-content;
-  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
   border-radius: 3px;
   padding: 1em;
 `;
 
 const Card = ({ children, ...props }: CardProps) => {
-    return <StyledCard {...props}>{children}</StyledCard>;
+  return <StyledCard {...props}>{children}</StyledCard>;
 };
 
 export default Card;
