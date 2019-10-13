b8237bcda300d8c17316c30206f6a0fb7272323b
plusk <andelar96@gmail.com>
2019-10-13 17:41:42 +0200

    Simplified and merged some styling

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,44 +2,27 @@ import React from 'react';
 import styled, { StyledComponentProps } from 'styled-components';
 import { colors } from 'common/colors';
 
-// Temporary until the Icon component is done
-class Icon {}
-
 interface ButtonProps extends StyledComponentProps<'button', any, any, any> {
   value?: React.ReactNode;
-  icon?: string | Icon;
   color?: 'primary' | 'secondary' | 'success' | 'danger';
   variant?: 'normal' | 'outline';
 }
 
 interface StyledButtonProps {
-  backgroundColor?: string;
-  borderColor?: string;
-  textColor?: string;
+  color?: string;
+  variant?: string;
   disabled?: boolean;
-  mainColor?: string;
-  boxShadow?: string;
 }
 
 const StyledButton = styled.button<StyledButtonProps>`
   appearance: none;
-  background: ${(props) => props.backgroundColor};
-  box-shadow: 0 2px 4px ${(props) => props.boxShadow};
-  color: ${(props) => props.textColor};
+  background: ${(props) => props.color};
+  border: 2px solid ${(props) => props.color};
+  color: ${colors.graysWhite};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   border-radius: 3px;
-  padding: 1em 2em;
-  margin: 4px;
-  text-transform: uppercase;
-  font-weight: bold;
-  min-width: 160px;
-  border: 2px solid ${(props) => props.borderColor};
+  padding: 0.5rem 1rem;
   cursor: pointer;
-
-  &:focus {
-    /* Not sure if this is what we want on focus. We should probably handle focus selectors elsewhere */
-    outline: none;
-    box-shadow: 0 2px 4px ${(props) => props.boxShadow}, 0 0 0 4px #fff8, 0 0 0 4px ${(props) => props.mainColor};
-  }
 
   &:hover {
     background-image: linear-gradient(0, #0001 0, #0001 0);
@@ -48,8 +31,18 @@ const StyledButton = styled.button<StyledButtonProps>`
   &:disabled {
     background: ${colors.grayslightGray};
     color: ${colors.graysGray};
-    border: none;
+    box-shadow: none;
+    border-color: ${colors.grayslightGray};
+    cursor: not-allowed;
   }
+
+  ${(props) =>
+    props.variant == 'outline' &&
+    `
+    background: transparent;
+    border: 2px solid ${props.color};
+    color: ${props.color};
+  `}
 `;
 
 const Button = ({
@@ -77,20 +70,10 @@ const Button = ({
   }
 
   const extraProps = {
-    backgroundColor: disabled ? '#0001' : mainColor,
-    borderColor: disabled ? 'transparent' : mainColor,
-    textColor: colors.graysWhite,
-    mainColor,
+    color: mainColor,
     disabled,
-    boxShadow: disabled ? 'transparent' : 'rgba(0, 0, 0, 0.1)',
+    variant,
   };
-  switch (variant) {
-    case 'outline': {
-      extraProps.backgroundColor = colors.graysWhite;
-      extraProps.textColor = mainColor;
-      break;
-    }
-  }
   return (
     <StyledButton {...props} {...extraProps}>
       {children || value}
