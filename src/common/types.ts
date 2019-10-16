33b164a229a0170743025f82c6bc1f319b1422ab
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-10-16 22:28:26 +0200

    use proper quotes

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,3 @@
-import { FlattenSimpleInterpolation } from "styled-components";
+import { FlattenSimpleInterpolation } from 'styled-components';
 
 export type ExternalCSS = FlattenSimpleInterpolation | undefined | false;
