6bffafd0164c45de62ba5e898b841c58fb4cc169
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-10-24 01:52:03 +0200

    Add css variables for fint size

--- src/common/global.ts
+++ src/common/global.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import Reset from './reset';
+import { media } from './layout';
 
 export const GlobalStyle = createGlobalStyle`
   ${Reset}
@@ -7,17 +8,31 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Source Sans Pro', sans-serif;
   }
+  :root {
+    --font-base-size: 1.2rem;
+    --font-scale: 1.4;
+    --font-size-xs: calc(var(--font-base-size) / (var(--font-scale) * var(--font-scale)));
+    --font-size-sm: calc(var(--font-base-size) / var(--font-scale));
+    --font-size-md: calc(var(--font-base-size));
+    --font-size-lg: calc(var(--font-base-size) * var(--font-scale));
+    --font-size-xl: calc(var(--font-base-size) * var(--font-scale) * var(--font-scale));
+    @media ${media.tabletAndUp} {
+      --font-base-size: 1.25rem;
+      --font-scale: 1.5;
+    }
+  }
   h1, h2, h3 {
     font-family: 'Source Serif Pro', serif;
     margin: 2rem 0 1rem;
+    line-height: 1.2;
   }
   h1 {
-    font-size: 2.5rem;
+    font-size: var(--font-size-xl);
   }
   h2 {
-    font-size: 2rem;
+    font-size: var(--font-size-lg);
   }
   h3 {
-    font-size: 1.25rem;
+    font-size: var(--font-size-md);
   }
 `;
