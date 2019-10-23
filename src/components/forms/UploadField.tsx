221bad2e244b7d71aac6c8581a3d9c15892f8abb
Børge S. Dreyer <borge.dreyer@gmail.com>
2019-10-23 23:40:28 +0200

    drag & drop fix

--- src/components/forms/UploadField.tsx
+++ src/components/forms/UploadField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { StyledComponentProps } from 'styled-components';
 import { colors } from 'common/colors';
 
@@ -7,7 +7,7 @@ interface UploadFieldProps extends StyledComponentProps<'input', any, any, any> 
 }
 
 const FileUpload = styled.input`
-  display: block;
+  display: none;
 `;
 const FileUploadLabel = styled.label`
   display: inline-block;
@@ -15,7 +15,7 @@ const FileUploadLabel = styled.label`
   text-align: center;
   background-color: ${colors.grayslightGray};
   box-sizing: border-box;
-  border: 2px dashed ${colors.primay};
+  border: 2px dashed ${colors.primary};
   width: 400px;
   padding: 20px 10px;
 `;
@@ -25,10 +25,39 @@ const FileUploadContainer = styled.div`
 const RandomId = 'upload' + Math.floor(Math.random() * 1000);
 
 const UploadField = ({ ...props }: UploadFieldProps) => {
+  const [text, setText] = useState('');
+
+  const fileChange = (files: any[]) => {
+    let build = '';
+    for (let file of files) {
+      build += ', ' + file.name;
+    }
+    setText(build.slice(2));
+  };
+
+  const handleFileChange = (evt: any) => {
+    if (evt.target && evt.target.files) {
+      fileChange(evt.target.files);
+    }
+  };
+
+  const handleDragOver = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    fileChange(e.dataTransfer.files);
+  };
+
   return (
     <FileUploadContainer>
-      <FileUpload id={RandomId} type="file" {...props} />
-      <FileUploadLabel htmlFor={RandomId}>{props.label}</FileUploadLabel>
+      <FileUpload id={RandomId} type="file" multiple onChange={handleFileChange} {...props} />
+      <FileUploadLabel htmlFor={RandomId} onDragOver={handleDragOver} onDrop={handleDrop}>
+        {props.label} {text}
+      </FileUploadLabel>
     </FileUploadContainer>
   );
 };
