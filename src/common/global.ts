cfcb2a1c07cf0a9c5544f02f6195e3a3f54e9221
plusk <andelar96@gmail.com>
2019-10-30 21:13:40 +0100

    Removed a lot of props in favor of inheritance

--- src/common/global.ts
+++ src/common/global.ts
@@ -3,8 +3,7 @@ import Reset from './reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${Reset}
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700|Source+Code+Pro');
-  @import url('https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp');
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700|Source+Code+Pro|Material+Icons+Two+Tone');
 
   body {
     font-family: 'Source Sans Pro', sans-serif;
@@ -22,8 +21,4 @@ export const GlobalStyle = createGlobalStyle`
   h3 {
     font-size: 1.25rem;
   }
-  .md-18 { font-size: 18px; }
-  .md-24 { font-size: 24px; }
-  .md-36 { font-size: 36px; }
-  .md-48 { font-size: 48px; }
 `;
--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -2,45 +2,17 @@ import React from 'react';
 import styled, { StyledComponentProps } from 'styled-components';
 
 interface IconProps extends StyledComponentProps<'i', any, any, any> {
-  icon: string;
-  color: string;
-  type?: 'outlined' | 'two-tone' | 'round' | 'sharp';
-  size: 'small' | 'medium' | 'large' | 'huge';
+  name: string;
 }
 
-interface StyledIconProps {
-  color: string;
-}
-
-const StyledIcon = styled.i<StyledIconProps>`
-  color: ${(props): string => props.color};
+const StyledIcon = styled.i`
+  color: inherit;
+  font-size: inherit;
+  vertical-align: bottom;
 `;
 
-const iconSizeEnum = {
-  small: '18',
-  medium: '24',
-  large: '36',
-  huge: '48',
-};
-
-const Icon = ({ icon, type, color, size }: IconProps): JSX.Element => {
-  const iconType = (): string => {
-    let base = 'material-icons';
-    if (type) {
-      return `${base}-${type}`;
-    }
-    return base;
-  };
-
-  const iconSize = (): string => {
-    return `md-${iconSizeEnum[size]}`;
-  };
-
-  return (
-    <StyledIcon color={color} className={`${iconType()} ${iconSize()}`}>
-      {icon}
-    </StyledIcon>
-  );
+const Icon = ({ name }: IconProps) => {
+  return <StyledIcon className="material-icons-two-tone">{name}</StyledIcon>;
 };
 
 export default Icon;
