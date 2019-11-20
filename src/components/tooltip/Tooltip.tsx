48e802d0215f00096bf3617f679a619f276ba8e4
mariush2 <marius@mh2.no>
2019-11-20 19:30:55 +0100

    unfinished still, some changes

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -12,15 +12,36 @@ const StyledTooltip = styled.div`
   color: white;
   position: absolute;
   padding: 12px 24px;
+  top: 105%;
+  left: -33%;
+  width: max-content;
   border-radius: 5px;
   margin-top: 5px;
   pointer-events: none;
+  text-align: center;
   opacity: 0;
   transition: opacity ease-in-out 250ms;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 6px solid transparent;
+    border-bottom-color: ${colors.graysGray};
+    border-top: 0;
+    margin-left: -6px;
+    margin-top: -6px;
+  }
 `;
 
 const Wrapper = styled.div`
   position: relative;
+  display: inline-block;
+  width: max-content;
+  height: max-content;
   :hover {
     > .tooltip {
       opacity: 1;
