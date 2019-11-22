2af276248258b42117de0a087c8d172a10a7ff39
plusk <andelar96@gmail.com>
2019-11-22 15:10:43 +0100

    Remove usage of tab

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 interface TimelineProps {
   children: React.ReactNode;
 }
 
 const Timeline = ({ children }: TimelineProps) => {
-  return <Fragment>{children}</Fragment>;
+  return <>{children}</>;
 };
 
 export default Timeline;
