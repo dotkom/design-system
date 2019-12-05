940a23e8b5ce3037ed531ce9f0e1eb2a16befb64
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-12-05 22:17:52 +0100

    Export info, ingress and paragraph directly

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -3,7 +3,6 @@ import styled, { StyledComponentProps, css } from 'styled-components';
 import { colors } from 'common/colors';
 import { Icon } from '../../';
 
-type TextProps = StyledComponentProps<'p', any, any, any>;
 interface QuoteProps extends StyledComponentProps<'blockquote', any, any, any> {
   by?: string;
 }
@@ -13,22 +12,25 @@ const TextStyle = css`
   line-height: 1.5;
 `;
 
-const StyledInfo = styled.p`
+export const Info = styled.p`
   font-size: var(--font-size-sm);
   ${TextStyle}
   color: ${colors.grayDarken60};
 `;
-const StyledIngress = styled.p`
+
+export const Ingress = styled.p`
   font-size: var(--font-size-md);
   margin: 2rem 0;
   font-style: italic;
   ${TextStyle}
 `;
-const StyledParagraph = styled.p`
+
+export const Paragraph = styled.p`
   font-size: var(--font-size-md);
   margin: 2rem 0;
   ${TextStyle}
 `;
+
 const StyledQuote = styled.blockquote`
   font-size: var(--font-size-lg);
   font-style: italic;
@@ -57,14 +59,6 @@ const StyledQuotedBy = styled.span`
     content: '- ';
   }
 `;
-
-export const Ingress = ({ children, ...props }: TextProps): JSX.Element => {
-  return <StyledIngress {...props}>{children}</StyledIngress>;
-};
-
-export const Paragraph = ({ children, ...props }: TextProps): JSX.Element => {
-  return <StyledParagraph {...props}>{children}</StyledParagraph>;
-};
 
 export const Quote: FC<QuoteProps> = ({ children, by = '', ...props }: QuoteProps): JSX.Element => {
   let source = null;
@@ -98,12 +92,8 @@ export const Quote: FC<QuoteProps> = ({ children, by = '', ...props }: QuoteProp
           <StyledQuotedBy>{source}</StyledQuotedBy>
         </>
       ) : (
-        ''
-      )}
+          ''
+        )}
     </StyledQuote>
   );
-};
-
-export const Info = ({ children, ...props }: TextProps): JSX.Element => {
-  return <StyledInfo {...props}>{children}</StyledInfo>;
 };
