cd2efaa9959448e935e9afd0ddbc197d383a2006
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-12-05 22:15:22 +0100

    Add H4-H6 and export dem direcly from styled components

--- src/components/typography/Heading.tsx
+++ src/components/typography/Heading.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import styled, { StyledComponentProps, css } from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors } from 'common/colors';
-
-type HeaderProps = StyledComponentProps<'h1', any, any, any>;
 
 const HeadingStyle = css`
   color: ${colors.grayDarken90};
 `;
 
-const StyledH1 = styled.h1`
+const Heading = styled.h1`
   ${HeadingStyle}
 `;
-const StyledH2 = styled.h2`
+const Heading2 = styled.h2`
   ${HeadingStyle}
 `;
-const StyledH3 = styled.h3`
+const Heading3 = styled.h3`
+  ${HeadingStyle}
+`;
+const Heading4 = styled.h4`
+  ${HeadingStyle}
+`;
+const Heading5 = styled.h5`
+  ${HeadingStyle}
+`;
+const Heading6 = styled.h6`
   ${HeadingStyle}
 `;
 
-export const Heading = ({ children, ...props }: HeaderProps): JSX.Element => {
-  return <StyledH1 {...props}>{children}</StyledH1>;
-};
-
-export const Heading2 = ({ children, ...props }: HeaderProps): JSX.Element => {
-  return <StyledH2 {...props}>{children}</StyledH2>;
-};
-
-export const Heading3 = ({ children, ...props }: HeaderProps): JSX.Element => {
-  return <StyledH3 {...props}>{children}</StyledH3>;
+export {
+  Heading,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
 };
