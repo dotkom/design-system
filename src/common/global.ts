240da0bdb321e3209fd80172b6a4b3b281221a70
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-12-07 15:34:00 +0100

    Change how fonts scale globally

--- src/common/global.ts
+++ src/common/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import Reset from './reset';
-import { media } from './layout';
+import { media, breakPointsInPX } from './layout';
 
 export const GlobalStyle = createGlobalStyle`
   ${Reset}
@@ -8,19 +8,33 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: 'Source Sans Pro', sans-serif;
+    line-height: 1.5;
   }
   :root {
     --font-base-size: 1.2rem;
-    --font-scale: 1.4;
-    --font-size-xxs: calc(var(--font-base-size) / (var(--font-scale) * var(--font-scale) * var(--font-scale)));
-    --font-size-xs: calc(var(--font-base-size) / (var(--font-scale) * var(--font-scale)));
-    --font-size-sm: calc(var(--font-base-size) / var(--font-scale));
-    --font-size-md: calc(var(--font-base-size));
-    --font-size-lg: calc(var(--font-base-size) * var(--font-scale));
-    --font-size-xl: calc(var(--font-base-size) * var(--font-scale) * var(--font-scale));
+    --font-min-scale: 1;
+    --font-max-scale: 1.2;
+    --font-size: calc(var(--font-base-size) * var(--font-min-scale));
+
+    --font-size-xxs: calc(var(--font-size) / (1.25 * 1.25 * 1.25));
+    --font-size-xs: calc(var(--font-size) / (1.25 * 1.25));
+    --font-size-sm: calc(var(--font-size) / 1.25);
+    --font-size-md: calc(var(--font-size));
+    --font-size-lg: calc(var(--font-size) * 1.25);
+    --font-size-xl: calc(var(--font-size) * 1.25 * 1.25);
+    --font-size-xxl: calc(var(--font-size) * 1.25 * 1.25 * 1.25);
+
+    /*
+    Borrowed from https://css-tricks.com/snippets/css/fluid-typography/:
+    font-size: calc([min-size] + ([max-size] - [min-size]) * ((100vw - [min-view-size]px) / ([max-view-size] - [min-view-size])))
+    */
     @media ${media.tabletAndUp} {
-      --font-base-size: 1.25rem;
-      --font-scale: 1.5;
+      --font-size: calc(var(--font-min-scale) * (var(--font-base-size) + (1.2 * 16) * (var(--font-max-scale) - var(--font-min-scale)) * ((100vw - ${
+        breakPointsInPX.minTabletWidth
+      }px) / ${breakPointsInPX.minDesktopWidth - breakPointsInPX.minTabletWidth})));
+    }
+    @media ${media.desktopAndUp} {
+      --font-size: calc(var(--font-base-size) * var(--font-max-scale));
     }
   }
   h1, h2, h3, h4, h5, h6 {
