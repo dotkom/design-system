dbe052e08df13559043c2750bd402ec0640263ba
carlofholy <carl.smestad@gmail.com>
2020-10-18 19:50:39 +0200

    feat(select): update select to use react-select
    Co-authored-by: FluidSense <johanneskv96@gmail.com>

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,28 +1,13 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import ReactSelect from 'react-select';
+import styled, { StyledComponentProps } from 'styled-components';
 import { colors } from 'common/colors';
-import Icon from 'components/icon/Icon';
 
-interface SelectProps {
-  children: React.ReactNode;
+interface SelectProps extends StyledComponentProps<'select', any, any, any> {
+  options: [{ value: any; label: any }];
 }
 
-const SelectContainer = styled.div`
-  display: inline;
-  position: relative;
-
-  & > *:last-child {
-    position: absolute;
-    font-size: 1.5rem;
-    top: 0;
-    right: 0;
-    pointer-events: none;
-    user-select: none;
-    color: ${colors.grayLighten60};
-  }
-`;
-
-const StyledSelect = styled.select`
+const StyledSelect = styled(ReactSelect)`
   transition: all 0.2s;
   padding: 8px 20px 8px 8px;
   background: #fff;
@@ -37,13 +22,8 @@ const StyledSelect = styled.select`
   appearance: none;
 `;
 
-const Select = ({ children }: SelectProps) => {
-  return (
-    <SelectContainer>
-      <StyledSelect>{children}</StyledSelect>
-      <Icon name="keyboard_arrow_down" />
-    </SelectContainer>
-  );
+const Select = ({ options, ...rest }: SelectProps) => {
+  return <StyledSelect options={options} {...rest} />;
 };
 
 export default Select;
