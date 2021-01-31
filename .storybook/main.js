de8d86fc3f46f453a72f370d61373eaf9a5c8ba7
kharann <anhkhav@gmail.com>
2021-01-31 18:18:15 +0100

    Update storybook to 6.1

---
+++ .storybook/main.js
@@ -0,0 +1,52 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-storysource/register',
+    '@storybook/addon-a11y/register',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          parser: 'typescript',
+          injectStoryParameters: false,
+        },
+      },
+    },
+  ],
+  typescript: {
+    check: true,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => ['label', 'disabled'].includes(prop.name),
+      //      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    if (config.resolve.plugins === undefined) {
+      config.resolve.plugins = [];
+    }
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
+};
