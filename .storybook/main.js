de1ef5c3bb224253f0102f143814787c2898aeee
kharann <anhkhav@gmail.com>
2021-01-31 19:14:13 +0100

    Update config to match newest storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,10 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path')
 
 module.exports = {
   stories: ['../src/components/**/*.stories.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-storysource/register',
-    '@storybook/addon-a11y/register',
     {
       name: '@storybook/addon-docs',
       options: {
@@ -29,24 +25,22 @@ module.exports = {
       //      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
-  webpackFinal: async (config) => {
+   webpackFinal: async (config) => {
     config.module.rules.push({
-      test: /\.tsx?$/,
-      use: [
-        {
-          loader: require.resolve('awesome-typescript-loader'),
-        },
-        // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
     });
-    config.resolve.extensions.push('.ts', '.tsx');
-    if (config.resolve.plugins === undefined) {
-      config.resolve.plugins = [];
-    }
-    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]]
+      }
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+
     return config;
-  },
+  }
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,45 @@
-import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-const input = 'src/index.ts';
-const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+const packageJson = require("./package.json");
 
-const plugins = [
-  typescript({
-    typescript: require('typescript'),
-  }),
-];
-
-export default [
-  {
-    input,
-    output: {
-      file: pkg.module,
-      format: 'esm',
-      sourcemap: true,
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true
     },
-    plugins,
-    external,
-  },
-  {
-    input,
-    output: {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins,
-    external,
-  },
-];
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss(),
+    /* copy({
+      targets: [
+        {
+          src: "src/variables.scss",
+          dest: "build",
+          rename: "variables.scss"
+        },
+        {
+          src: "src/typography.scss",
+          dest: "build",
+          rename: "typography.scss"
+        }
+      ]
+    }) */
+  ]
+};
